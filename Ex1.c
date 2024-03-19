5b8336cb11c99c83c9b7842261cbad3123a39042
Jana Wael <janawael@gmail.com>
2024-03-19 02:01:23 +0200

    Homework4

---
+++ Ex1.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main()
+{
+	int x,y;
+	printf("Enter number 1 : ");
+	scanf(" %d",&x);
+	fflush(stdin);
+	printf("Enter number 2 : ");
+	scanf(" %d",&y);
+
+	for(int i = x ; i <= y ; i++)
+	{
+	    int flag = 0;
+		for(int j = 2 ; j < i; j++)
+		{
+			if(i%j==0)
+			{
+				flag = 1;
+				break;
+			}
+		}
+		if(flag==0)
+			printf("\n%d is a prime number",i);
+	}
+
+}
